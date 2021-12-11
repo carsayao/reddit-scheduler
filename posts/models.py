0e156423ea4144ed79b7d1785563cca1d80bae77
Carlos Miguel Sayao <carlossayao@gmail.com>
2021-12-10 20:18:47 -0800

    left off on 'playing with the api'

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,26 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=20)
     #timezone
+    def __str__(self):
+        return self.username
 
 class Content(models.Model):
+    # Unverified accounts, max=150
     default_title = models.CharField(max_length=300)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     kind = models.CharField(max_length=200)
     creation_date = models.DateTimeField()
+    def __str__(self):
+        return self.default_title
+        # return self.default_title, self.user, self.kind, self.creation_date
+    def was_published_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days-1)
 
 class Post(models.Model):
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
@@ -19,8 +30,13 @@ class Post(models.Model):
     send_replies = models.BooleanField(null=False)
     flair = models.CharField(max_length=200, blank=True)
     reddit_link = models.URLField(blank=True)
+    def __str__(self):
+        return self.override_title
+        # return self.override_title, self.subreddit, self.strat
 
 class Strategy(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     strat_type = models.CharField(max_length=200)
     strat_date = models.DateTimeField(blank=True)
+    def __str__(self):
+        return self.strat_type
