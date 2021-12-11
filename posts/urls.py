f63a769e5aa1b2c6b9ad77783a25c2df7b5689b9
Carlos Miguel Sayao <carsayao@pdx.edu>
2021-12-11 03:23:10 -0800

    Finished part 3

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+# Set application namespace
+app_name = 'posts'
 urlpatterns = [
+    # /posts/
     path('', views.index, name='index'),
+    # /posts/5/
+    path('<int:user_id>/', views.detail, name='detail'),
+    # /posts/5/user/
+    path('<int:user_id>/user/', views.user, name='user')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+    username_list = User.objects.all()
+    template = loader.get_template('posts/index.html')
+    # The context is a dictionary mapping template variable names to
+    # Python objects.
+    context = {
+        'username_list': username_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'posts/index.html', context)
+
+# Raise the Http404 exception if a user with the requested ID doesn’t exist.
+def detail(request, user_id):
+    # return HttpResponse("You're looking at user %s." % user_id)
+
+    # try:
+    #     user = User.objects.get(pk=user_id)
+    # except User.DoesNotExist:
+    #     raise Http404("User does not exist")
+    # return render(request, 'posts/detail.html', {'user': user})
+
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'posts/detail.html', {'user': user})
+
+def user(request, user_id):
+    return HttpResponse("You're looking at the name of %s." % user_id)
