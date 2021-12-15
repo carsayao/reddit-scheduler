8f4f85de8f08119cfb7cbbc20a6c7bfd431e1916
Carlos Miguel Sayao <carlossayao@gmail.com>
2021-12-14 23:36:22 -0800

    finished part 3 for polls

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'poster.apps.PosterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('poster/', include('poster.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
