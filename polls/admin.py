938adf971ad96a550e490db8880dc56cd0d9194b
Carlos Miguel Sayao <carsayao@pdx.edu>
2021-12-21 08:46:59 -0800

    finished part 7 for polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):    # More compact
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Move published date above question text
+    # fields = ['pub_date', 'question_text']
+
+    # Pretty fieldset form
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+# Inefficient way of adding Choice objects to system. It’d be better if
+# you could add a bunch of Choices directly when you create the Question object
+# admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -2,14 +2,23 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
+
+    # Improved display
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         # Buggy code returns true even if pub_date in future
         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
