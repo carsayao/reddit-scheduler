fb87e0a4ef325e066e1b9764341c977013a04d49
Carlos Miguel Sayao <carlossayao@gmail.com>
2022-01-31 18:32:04 -0800

    started posts list view
    Need to take different approach:
    - list posts in content_detail.html
    - move content_posts.html contents into content_detail.html

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,4 @@
+from re import template
 from django.http import HttpResponse
 from django.http import Http404
 from django.http.response import HttpResponseRedirect
@@ -9,7 +10,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 
 from .forms import ContentForm
-from .models import User, Content
+from .models import User, Content, Post
 
 class IndexView(ListView):
     template_name = 'scheduler/index.html'
@@ -33,3 +34,10 @@ class ContentDetailView(DetailView):
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     return context
+
+class PostListView(ListView):
+    template_name = 'scheduler/content_posts.html'
+    context_object_name = 'post_list'
+    def get_queryset(self):
+        self.content = get_object_or_404(Content, name=self.kwargs['content'])
+        return Post.objects.select_relate().filter(content=self.content)
